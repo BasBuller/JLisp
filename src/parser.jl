4ed59056f9e1ae3b247a75c5d1ce591fb83b9844
Bas Buller <bas.buller@gmail.com>
2023-10-25 08:36:13 +0100

    Make basic types way easier

--- src/parser.jl
+++ src/parser.jl
@@ -1,20 +1,3 @@
-abstract type SchemeObject end
-struct SNumber{T<:Number} <: SchemeObject
-    value::T
-end
-struct SString <: SchemeObject
-    value::String
-end
-struct SChar <: SchemeObject
-    value::Char
-end
-struct SBool <: SchemeObject
-    value::Bool
-end
-struct SSymbol <: SchemeObject
-    value::Symbol
-end
-struct Pair{V<:SchemeObject, W<:SchemeObject} <: SchemeObject
-    car::V
-    cdr::W
-end
+include("lexer.jl")
+
+SchemeObject = Union{Number, String, Char, Bool, Symbol, Pair}

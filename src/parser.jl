a2d29b59908dd3063c95bf1524f645b40c006e2b
Bas Buller <bas.buller@gmail.com>
2023-10-25 08:57:06 +0100

    basic parsing routine

--- src/parser.jl
+++ src/parser.jl
@@ -1,3 +1,22 @@
 include("lexer.jl")
 
 SchemeObject = Union{Number, String, Char, Bool, Symbol, Pair}
+
+struct Parser
+    currentToken
+    lexer::Lexer
+    
+    function Parser(code_string::String)
+        lexer = Lexer(code_string)
+        return new(nothing, lexer)
+    end
+end
+getToken(parser::Parser) = getToken(parser.lexer)
+
+function nextToken(parser::Parser)
+    token = getToken(parser)
+    while !isnothing(token) & (token.token_type == SComment)
+        token = getToken(parser)
+    end
+    return token
+end
